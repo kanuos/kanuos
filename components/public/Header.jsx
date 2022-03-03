6b89e5202c1b97b8a1ffa9a6f6abec9c305d1e8d
kanuos <sounakmukherjee@ymail.com>
2022-03-02 20:52:42 -0500

    Project dynamic list and detail body

--- components/public/Header.jsx
+++ components/public/Header.jsx
@@ -1,13 +1,17 @@
+import { JoinLine } from "./DescHeader";
+
 export const PublicHeader = ({data}) => {
     const {title, desc, count, type} = data;
     return (
-    <header className="flex flex-col items-start mb-16 text-dark">
-        <h1 className="font-special font-semibold text-4xl mb-6 capitalize">
+    <header className="flex flex-col items-start mb-16 gap-y-2 text-dark">
+        <h1 className="font-special font-semibold text-4xl capitalize">
             {title}
         </h1>
+        <JoinLine />
         <p className="text-sm whitespace-pre-line break-words opacity-75 max-w-xl">{desc}</p>
+        {count > 0 &&
         <strong className="capitalize text-xs font-semibold mt-4">
             total {type} : {count}
-        </strong>
+        </strong>}
     </header>
 )}
