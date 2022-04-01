b5b60db1ff79ee7e513df82708878bd75885af2c
kanuos <sounakmukherjee@ymail.com>
2022-04-01 19:18:28 -0400

    reduced video bg opacity

--- components/public/VideoBG.jsx
+++ components/public/VideoBG.jsx
@@ -1,14 +1,15 @@
 export const VideoBG = () => {
   return (
-    <video 
-        muted
-        autoPlay
-        loop
-        poster='/hero.webp'
-        playsInline={true}
-        className='h-screen w-screen object-cover block pointer-events-none opacity-20 fixed inset-0'>
-        <source src='/pf.webm' type='video/webm'/>
-        <source src='/pf.mp4' type='video/mp4'/>
+    <video
+      muted
+      autoPlay
+      loop
+      poster="/hero.webp"
+      playsInline={true}
+      className="h-screen w-screen object-cover block pointer-events-none opacity-10 fixed inset-0"
+    >
+      <source src="/pf.webm" type="video/webm" />
+      <source src="/pf.mp4" type="video/mp4" />
     </video>
-  )
-}
+  );
+};
