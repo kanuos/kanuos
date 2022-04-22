131d9de09b931d14426471feeb0376c5c6b23c62
kanuos <sounakmukherjee@ymail.com>
2022-04-22 17:45:47 -0400

    CTA button

---
+++ components/portfolio/CTA.jsx
@@ -0,0 +1,46 @@
+// built in imports
+import Link from "next/link";
+
+export const CTA = ({
+  label,
+  href,
+  btnMode = false,
+  cb = undefined,
+  externalLink = false,
+  isDarkMode = false,
+}) => {
+  const wrapper =
+    "text-sm md:text-base font-semibold relative block group overflow-hidden hover:shadow-2xl transition-all";
+  const content = `z-10 border-2 w-full h-full block py-1.5 px-6 relative rounded-full transition-all ${
+    isDarkMode
+      ? "group-hover:text-dark border-light"
+      : "group-hover:text-light border-dark"
+  }`;
+  const style = `absolute left-0 top-full group-hover:top-0 group-hover:scale-100 scale-0 origin-center transition-all w-full h-full rounded-full ease-linear ${
+    isDarkMode ? "bg-light" : "bg-dark"
+  }`;
+  if (btnMode) {
+    return (
+      <button className={wrapper} onClick={cb}>
+        <span className={content}>{label}</span>
+        <span className={style}></span>
+      </button>
+    );
+  }
+  if (externalLink) {
+    return (
+      <a className={wrapper} href={href} target="_blank">
+        <span className={content}>{label}</span>
+        <span className={style}></span>
+      </a>
+    );
+  }
+  return (
+    <Link href={href}>
+      <a className={wrapper}>
+        <span className={content}>{label}</span>
+        <span className={style}></span>
+      </a>
+    </Link>
+  );
+};
