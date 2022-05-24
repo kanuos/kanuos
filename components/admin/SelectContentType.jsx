076defb82e2ab8f0b8b042410e92da826903870a
kanuos <sounakmukherjee@ymail.com>
2022-05-23 23:25:10 -0400

    tag selector + select content type

--- components/admin/SelectContentType.jsx
+++ components/admin/SelectContentType.jsx
@@ -1,50 +1,39 @@
 import { useState } from "react";
 import { CONTENT_TYPE } from "../../utils/admin";
-import { CTA } from "../portfolio/CTA";
 
 export const SelectContentType = ({
   getContentType,
-  init = "",
+  type = "",
   isDarkMode,
 }) => {
-  const [type, setType] = useState(init);
   return (
-    <div className="h-full w-full flex items-start justify-center max-w-prose mx-auto">
-      <section className="flex flex-col items-stretch w-full gap-y-2">
-        <h1 className="heading--main capitalize mb-10">select content type</h1>
-        <div className="flex flex-col items-start justify-around gap-4 my-10 w-full">
-          {Object.entries(CONTENT_TYPE).map(([k, { name }]) => (
-            <label
-              key={k}
-              className={`inline-flex flex-row-reverse items-center justify-end gap-x-1 opacity-75 hover:opacity-100 cursor-pointer transition-opacity peer-focus:opacity-100 ${
-                isDarkMode ? "bg-secondary" : "bg-primary"
-              } bg-opacity-10 p-4 w-full`}
-            >
-              <span className="text-sm capitalize font-semibold">{name}</span>
-              <input
-                checked={type === name}
-                type="radio"
-                onChange={() => setType(name)}
-                name="type"
-                value={name}
-                className={`${
-                  isDarkMode ? "accent-secondary" : "accent-primary"
-                } cursor-pointer outline-none focus:outline-none peer`}
-              />
-            </label>
-          ))}
-        </div>
-        {Boolean(type) && (
-          <div className="w-max capitalize">
-            <CTA
-              isDarkMode={isDarkMode}
-              btnMode={true}
-              cb={() => getContentType(type)}
-              label={`Proceed with "${type}" →`}
+    <div className="h-full w-full flex items-start justify-center">
+      <div className="flex flex-col items-start justify-around gap-4 w-full my-6">
+        {Object.entries(CONTENT_TYPE).map(([k, { name }]) => (
+          <label
+            key={k}
+            className={`inline-flex flex-row-reverse items-center justify-end gap-x-1 opacity-75 hover:opacity-100 cursor-pointer transition-opacity peer-focus:opacity-100 border-2 rounded-md ${
+              name === type
+                ? isDarkMode
+                  ? "bg-secondary border-secondary"
+                  : "bg-primary border-primary"
+                : ""
+            } bg-opacity-10 p-4 w-full`}
+          >
+            <span className="text-sm capitalize font-semibold">{name}</span>
+            <input
+              checked={type === name}
+              type="radio"
+              onChange={() => getContentType(name)}
+              name="type"
+              value={name}
+              className={`${
+                isDarkMode ? "accent-secondary" : "accent-primary"
+              } cursor-pointer outline-none focus:outline-none peer`}
             />
-          </div>
-        )}
-      </section>
+          </label>
+        ))}
+      </div>
     </div>
   );
 };
--- components/admin/TagSelector.jsx
+++ components/admin/TagSelector.jsx
@@ -1,49 +1,25 @@
 import { useRouter } from "next/router";
 import { ADMIN_URLS } from "../../utils";
-import { CTA } from "../portfolio/CTA";
 import { Tag } from "../public/Tag";
 
-export const TagSelector = ({
-  handleTag,
-  selectedTags,
-  next,
-  allTags,
-  prev,
-}) => {
+export const TagSelector = ({ handleTag, selectedTags, allTags }) => {
   const router = useRouter();
   if (allTags.length === 0) {
     router.replace(ADMIN_URLS.tags.url);
     return <></>;
   }
   return (
-    <div className="section h-full w-full flex items-start justify-center">
-      <section className="flex flex-col items-start gap-y-2  w-full max-w-2xl">
-        <h1 className="heading--main">Select Tags</h1>
-        <p className="content--main">
-          Total selected tags : {selectedTags.length}
-        </p>
-        <div className="flex flex-wrap gap-4 items-center justify-start mt-4 mb-10">
-          {allTags.map((t) => {
-            let isPresent = Boolean(
-              selectedTags.find((selected) => selected._id === t._id)
-            );
-            return (
-              <div className="w-max" key={t._id}>
-                <Tag tag={t} cb={() => handleTag(t)} isActive={isPresent} />
-              </div>
-            );
-          })}
-        </div>
-
-        <ul className="flex items-center justify-start gap-x-8">
-          <li>{prev && <CTA label="← Prev" cb={prev} btnMode={true} />}</li>
-          <li>
-            {selectedTags.length > 0 && (
-              <CTA label="Next →" cb={next} btnMode={true} />
-            )}
-          </li>
-        </ul>
-      </section>
+    <div className="flex flex-wrap gap-4 items-center justify-start pt-8 pb-4 w-full">
+      {allTags.map((t) => {
+        let isPresent = Boolean(
+          selectedTags.find((selected) => selected._id === t._id)
+        );
+        return (
+          <div className="w-max" key={t._id}>
+            <Tag tag={t} cb={() => handleTag(t)} isActive={isPresent} />
+          </div>
+        );
+      })}
     </div>
   );
 };
