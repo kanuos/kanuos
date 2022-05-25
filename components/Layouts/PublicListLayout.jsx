f47688059bd898ecf6c46195ef0e9e26c87dfaec
kanuos <sounakmukherjee@ymail.com>
2022-05-25 16:43:40 -0400

    adjusted padding top

--- components/Layouts/PublicListLayout.jsx
+++ components/Layouts/PublicListLayout.jsx
@@ -7,10 +7,10 @@ export const PublicListLayout = ({ pageTitle, pageDesc, data, children }) => {
   return (
     <Fragment>
       <PublicLayout metaTitle={pageTitle} metaDesc={pageDesc}>
-        <div className="p-8 lg:px-0 max-w-2xl mx-auto select-text">
+        <div className="p-8 pt-16 lg:px-0 max-w-2xl mx-auto select-text">
           <PublicHeader data={data} />
         </div>
-        <div className="p-8 w-full min-h-[53vh] mx-auto flex flex-col items-stretch">
+        <div className="p-8 w-full min-h-[50vh] mx-auto flex flex-col items-stretch">
           {children}
           <div className="mt-auto">
             <Footer />
