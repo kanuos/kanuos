edabd1a553725c2c1fae65465d1fec93fa9262e9
kanuos <sounakmukherjee@ymail.com>
2022-06-23 15:48:38 -0400

    design thumbnail

--- components/content/DesignThumbnail.jsx
+++ components/content/DesignThumbnail.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { PUBLIC_NAVIGATION_URLS } from "../../utils";
+import { PUBLIC_NAVIGATION_URLS, titleCase } from "../../utils";
 import { motion } from "framer-motion";
 import { ADMIN_EDIT_URL } from "../../utils/admin";
 import { CTA } from "../portfolio/CTA";
@@ -9,57 +9,66 @@ export const DesignThumbnail = ({ data, adminMode = false, index = 0 }) => {
   const designURL = adminMode
     ? ADMIN_EDIT_URL("design", data._id)
     : PUBLIC_NAVIGATION_URLS.designs + "/" + data.slug;
-  return (
-    <div className="flex flex-col items-center justify-center gap-y-6">
-      <motion.article
-        initial={{ scale: 0.5 }}
-        whileInView={{
-          scale: 1.05,
-          transition: { type: "spring", stiffness: 400 },
-        }}
-        whileHover={{
-          scale: 1.1,
-          rotate: index % 2 ? 1 : -1,
-          transition: { type: "spring", stiffness: 400 },
-          shadow: "1px 4px 10px rgba(0,0,0,.10)",
-        }}
-        whileTap={{ scale: 1.1 }}
-        className="text-dark bg-light sm:hover:scale-110 flex flex-col gap-y-2 relative h-full mx-auto w-64 lg:h-full flex-grow group overflow-hidden p-2 shadow-lg hover:shadow-2xl"
-      >
-        <div className="bg-dark md:bg-light w-full h-full min-h-[22rem] max-w-xs">
-          <figure className="relative w-full h-full min-h-[22rem] max-w-xs">
-            <Image
-              loader={({ src, width }) => `${src}?w=${width}&q=75`}
-              src={data.thumbnail}
-              layout="fill"
-              alt={data.title + "'s thumbnail"}
-              className="h-full w-full object-cover filter group-hover:blur-sm md:group-hover:blur-none transition-all group-hover:h-3/5 group-hover:scale-125"
-            />
-          </figure>
-        </div>
 
-        <section className="bottom-0 left-0 w-full h-full md:h-3/5 absolute z-10 bg-transparent group-hover:bg-light opacity-0 group-hover:opacity-100 group-hover:bg-opacity-70 md:group-hover:bg-opacity-100 md:bg-light bg-opacity-90 md:bg-opacity-100 translate-y-0 md:translate-y-full md:group-hover:translate-y-0 md:opacity-0 md:group-hover:opacity-100 md:transition-all md:transform-gpu md:duration-300 md:delay-200 flex flex-col items-center justify-center gap-y-10">
-          <h3 className="heading--sub w-min text-center text-light group-hover:text-dark md:text-dark">
-            {data.title}
-          </h3>
-          <div className="hover:-translate-y-1 transition-all scale-0 group-hover:scale-90 md:scale-90 hover:scale-x-100">
-            <CTA
-              label={adminMode ? "View in Admin mode" : "Explore design"}
-              href={designURL}
-              isDarkMode={false}
-            />
-          </div>
-        </section>
-      </motion.article>
+  return (
+    <motion.article
+      initial={{ scale: 0.75, rotate: index % 2 === 0 ? 10 : -10 }}
+      whileInView={{
+        scale: 1.01,
+        rotate: 0,
+        transition: { type: "tween", stiffness: 200 },
+      }}
+      className="max-h-[55vh] h-full text-dark bg-light sm:hover:scale-110 grid grid-rows-2 relative mx-auto max-w-md w-full lg:h-full flex-grow group p-1.5 shadow-lg overflow-hidden hover:shadow-2xl rounded-md"
+    >
+      {/* admin tag */}
+
       {adminMode && (
         <p
-          className={`font-semibold content--sub text-center ${
-            data.isPublic ? "text-secondary" : "text-primary"
-          }`}
+          className={`absolute top-4 left-0 w-fit mx-auto font-semibold content--sub text-center bg-dark text-light p-4 z-20`}
         >
-          <small>Access : {!data.isPublic ? "Private" : "Public"}</small>
+          {!data.isPublic ? "Private" : "Public"}
         </p>
       )}
-    </div>
+
+      {/* card image */}
+      <figure className="relative w-full h-full row-start-1 col-span-full z-10 group-hover:row-end-2 row-end-3 rounded-md overflow-hidden transition">
+        <Image
+          loader={({ src, width }) => `${src}?w=${width}&q=100`}
+          src={data.thumbnail}
+          layout="fill"
+          objectFit="cover"
+          className="transition-all"
+          alt={data.title + "'s thumbnail"}
+        />
+      </figure>
+
+      {/* card details */}
+      <section
+        className={`transition-all transform-gpu duration-300 delay-200 grid grid-rows-6 col-span-full px-4 pb-6 pt-2 row-start-2 row-end-full z-0 group-hover:opacity-100 group-hover:pointer-events-auto opacity-0 pointer-events-none`}
+      >
+        <div className="flex row-span-1 h-full items-center justify-start">
+          <p className="text-xs">
+            <small className="bg-primary text-light px-1 py-0.5 rounded font-semibold capitalize">
+              {data.category}
+            </small>
+          </p>
+        </div>
+        <h3 className="heading--sub row-start-2 row-end-3 text-dark mt-1">
+          {titleCase(data.title)}
+        </h3>
+        <p className="content--secondary row-start-3 row-end-6 text-ellipsis">
+          {titleCase(data.desc).slice(0, 50) +
+            (data.desc.length > 50 ? "..." : "")}
+        </p>
+        <div className="w-max row-start-6 row-end-7 hover:-translate-y-1 drop-shadow-xl transition-all scale-90 origin-left">
+          <CTA
+            label={adminMode ? "View in Admin mode" : "Explore design"}
+            href={designURL}
+            isDarkMode={false}
+            isActive={true}
+          />
+        </div>
+      </section>
+    </motion.article>
   );
 };
--- components/public/TagDetailList.jsx
+++ components/public/TagDetailList.jsx
@@ -1,161 +1,192 @@
 import { useEffect, useState, useContext } from "react";
 
-import { IoCloseOutline, IoPricetagOutline } from "react-icons/io5"
+import { IoCloseOutline, IoPricetagOutline } from "react-icons/io5";
 
-import { BlogThumbnail } from "../content/BlogThumbnail"
-import { ProjectThumbnail } from "../content/ProjectThumbnail"
-import { DesignThumbnail } from "../content/DesignThumbnail"
+import { BlogThumbnail } from "../content/BlogThumbnail";
+import { ProjectThumbnail } from "../content/ProjectThumbnail";
+import { DesignThumbnail } from "../content/DesignThumbnail";
 import { JoinLine } from "./DescHeader";
 
 import { CONTENT_TYPE } from "../../utils/admin";
 import { ThemeContext } from "../../contexts/ThemeContext";
 
+export const TagDetailList = (props) => {
+  const { tag, project, blog, design, close } = props;
+  const [active, setActive] = useState("");
 
+  const { isDarkMode } = useContext(ThemeContext);
 
-export const TagDetailList = props => {
-    const {tag, project, blog, design, close} = props;
-    const [active, setActive] = useState('');
+  useEffect(() => {
+    if (project.length > 0) {
+      setActive(CONTENT_TYPE.project.name);
+      return;
+    }
+    if (blog.length > 0) {
+      setActive(CONTENT_TYPE.blog.name);
+      return;
+    }
+    if (design.length > 0) {
+      setActive(CONTENT_TYPE.design.name);
+      return;
+    }
+  }, [project.length, blog.length, design.length]);
 
-    const { isDarkMode } = useContext(ThemeContext);
-
-    useEffect(() => {
-        if (project.length > 0) {
-            setActive(CONTENT_TYPE.project.name)
-            return;
-        }
-        if (blog.length > 0) {
-            setActive(CONTENT_TYPE.blog.name)
-            return;
-        }
-        if (design.length > 0) {
-            setActive(CONTENT_TYPE.design.name)
-            return;
-        }
-    }, [project.length, blog.length, design.length])
-
-
-    return (
-    <section className='flex flex-col items-start gap-4'>
-        <div className={`p-4 rounded-md flex items-center justify-between filter drop-shadow-xl mt-10 max-w-3xl mx-auto w-full ${isDarkMode ? 'nav-light' : 'nav-dark'}`}>
-            <p className="font-semibold text-xs">
-                Searching for <span className="text-primary capitalize text-sm ml-1">{tag}</span>
-            </p>
-            <button onClick={close} className="hover:rotate-90 hover:text-primary hover:scale-110 transition-all">
-                <IoCloseOutline />
+  return (
+    <section className="flex flex-col items-start gap-4">
+      <div
+        className={`p-4 rounded-md flex items-center justify-between filter drop-shadow-xl mt-10 max-w-3xl mx-auto w-full ${
+          isDarkMode ? "nav-light" : "nav-dark"
+        }`}
+      >
+        <p className="font-semibold text-xs">
+          Searching for{" "}
+          <span className="text-primary capitalize text-sm ml-1">{tag}</span>
+        </p>
+        <button
+          onClick={close}
+          className="hover:rotate-90 hover:text-primary hover:scale-110 transition-all"
+        >
+          <IoCloseOutline />
+        </button>
+      </div>
+      <ul className="max-w-3xl mx-auto w-full flex items-center mt-10 justify-center gap-4">
+        {blog.length > 0 && (
+          <li
+            className={
+              active === CONTENT_TYPE.blog.name
+                ? "opacity-100 underline underline-offset-4"
+                : "opacity-40"
+            }
+          >
+            <button
+              className="font-semibold text-xs capitalize"
+              onClick={() => setActive(CONTENT_TYPE.blog.name)}
+            >
+              {CONTENT_TYPE.blog.name}
             </button>
-        </div>
-        <ul className="max-w-3xl mx-auto w-full flex items-center mt-10 justify-center gap-4">
-            {blog.length > 0 && 
-            <li className={(active === CONTENT_TYPE.blog.name ? "opacity-100 underline underline-offset-4" : "opacity-40")}>
-                <button 
-                    className="font-semibold text-xs capitalize" 
-                    onClick={() => setActive(CONTENT_TYPE.blog.name)}>
-                    {CONTENT_TYPE.blog.name}
-                </button>
-            </li>}
-            {project.length > 0 &&
-            <li className={(active === CONTENT_TYPE.project.name ? "opacity-100 underline underline-offset-4" : "opacity-40")}>
-                <button 
-                    className="font-semibold text-xs capitalize" 
-                    onClick={() => setActive(CONTENT_TYPE.project.name)}>
-                    {CONTENT_TYPE.project.name}
-                </button>
-            </li>}
-            {design.length > 0 && 
-            <li className={(active === CONTENT_TYPE.design.name ? "opacity-100 underline underline-offset-4" : "opacity-40")}>
-                <button 
-                    className="font-semibold text-xs capitalize" 
-                    onClick={() => setActive(CONTENT_TYPE.design.name)}>
-                    {CONTENT_TYPE.design.name}
-                </button>
-            </li>}
-        </ul>
-        {(active === CONTENT_TYPE.project.name) && <GroupList type={CONTENT_TYPE.project.name} list={project} tag={tag} />}
-        {(active === CONTENT_TYPE.blog.name) && <GroupList type={CONTENT_TYPE.blog.name} list={blog} tag={tag} />}
-        {(active === CONTENT_TYPE.design.name) && <GroupList type={CONTENT_TYPE.design.name} list={design} tag={tag} />}
-
-        {!Boolean(project.length || blog.length || design.length) && (
-            <p className="text-center flex items-start justify-center gap-1 w-full my-10">
-                <span>
-                No content found for 
-                </span>
-                <strong className="text-primary capitalize underline underline-offset-4 font-semibold text-sm">{tag}</strong>
-            </p>
+          </li>
         )}
+        {project.length > 0 && (
+          <li
+            className={
+              active === CONTENT_TYPE.project.name
+                ? "opacity-100 underline underline-offset-4"
+                : "opacity-40"
+            }
+          >
+            <button
+              className="font-semibold text-xs capitalize"
+              onClick={() => setActive(CONTENT_TYPE.project.name)}
+            >
+              {CONTENT_TYPE.project.name}
+            </button>
+          </li>
+        )}
+        {design.length > 0 && (
+          <li
+            className={
+              active === CONTENT_TYPE.design.name
+                ? "opacity-100 underline underline-offset-4"
+                : "opacity-40"
+            }
+          >
+            <button
+              className="font-semibold text-xs capitalize"
+              onClick={() => setActive(CONTENT_TYPE.design.name)}
+            >
+              {CONTENT_TYPE.design.name}
+            </button>
+          </li>
+        )}
+      </ul>
+      {active === CONTENT_TYPE.project.name && (
+        <GroupList type={CONTENT_TYPE.project.name} list={project} tag={tag} />
+      )}
+      {active === CONTENT_TYPE.blog.name && (
+        <GroupList type={CONTENT_TYPE.blog.name} list={blog} tag={tag} />
+      )}
+      {active === CONTENT_TYPE.design.name && (
+        <GroupList type={CONTENT_TYPE.design.name} list={design} tag={tag} />
+      )}
+
+      {!Boolean(project.length || blog.length || design.length) && (
+        <p className="text-center flex items-start justify-center gap-1 w-full my-10">
+          <span>No content found for</span>
+          <strong className="text-primary capitalize underline underline-offset-4 font-semibold text-sm">
+            {tag}
+          </strong>
+        </p>
+      )}
     </section>
-    
-  )
-}
+  );
+};
 
+const GroupList = ({ type, list, tag }) => {
+  return (
+    <section className="my-10 w-full">
+      <div className="my-10 flex flex-col items-center justify-center gap-2">
+        <p className="flex items-center justify-center gap-1 opacity-50">
+          <IoPricetagOutline />
+          <span className="capitalize font-semibold text-xs">{tag}</span>
+        </p>
+        <h3 className="capitalize font-black text-center text-3xl md:text-5xl">
+          {type}
+        </h3>
+        <p className="text-xs text-primary font-semibold ml-1">
+          <small>
+            Total {type} : {list.length}
+          </small>
+        </p>
+        <JoinLine />
+      </div>
+      {type === CONTENT_TYPE.project.name && (
+        <div className="flex flex-col items-stretch w-11/12 gap-y-20 max-w-5xl mx-auto">
+          {list?.map((project, index) => (
+            <ProjectThumbnail
+              key={project._id}
+              data={project}
+              index={index + 1}
+            />
+          ))}
+        </div>
+      )}
 
+      {type === CONTENT_TYPE.blog.name && (
+        <div className="flex flex-col items-stretch w-11/12 gap-y-20 max-w-5xl mx-auto">
+          {list?.map((blog, index) => (
+            <BlogThumbnail key={blog._id} data={blog} index={index + 1} />
+          ))}
+        </div>
+      )}
 
-const GroupList = ({type, list, tag}) => {
-    return (
-        <section className="my-10 w-full">
-            <div className="my-10 flex flex-col items-center justify-center gap-2">
-                <p className="flex items-center justify-center gap-1 opacity-50">
-                    <IoPricetagOutline />
-                    <span className="capitalize font-semibold text-xs">{tag}</span>
-                </p>
-                <h3 className="capitalize font-black text-center text-3xl md:text-5xl">{type}</h3>
-                <p className="text-xs text-primary font-semibold ml-1">
-                    <small>
-                    Total {type} : {list.length}
-                    </small>
-                </p> 
-                <JoinLine />
+      {type === CONTENT_TYPE.design.name && (
+        <>
+          {list.length < 4 && (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-20 p-4 w-full mb-20 max-w-7xl mx-auto justify-center items-center">
+              {list.map((design, i) => (
+                <DesignThumbnail
+                  key={design._id}
+                  data={design}
+                  index={i}
+                  center={list.length < 4}
+                />
+              ))}
             </div>
-            {(type === CONTENT_TYPE.project.name) && 
-                <div className="flex flex-col items-stretch w-11/12 gap-y-20 max-w-3xl mx-auto">
-                {list?.map((project, index) => (
-                    <ProjectThumbnail 
-                        key={project._id} 
-                        data={project} 
-                        index={index + 1} />
-                ))}
-                </div>
-            }
-            
-            {(type === CONTENT_TYPE.blog.name) && 
-                <div className="flex flex-col items-stretch w-11/12 gap-y-20 max-w-3xl mx-auto">
-                    {list?.map((blog, index) => (
-                    <BlogThumbnail 
-                        key={blog._id} 
-                        data={blog} 
-                        index={index + 1} />
-                ))}
-                </div>
-            }
-
-            {(type === CONTENT_TYPE.design.name) && 
-                <>
-                {list.length < 4 && (
-                <div className="grid grid-flow-row grid-cols-1 place-items-center gap-20 p-4 w-full mb-20 max-w-5xl mx-auto">
-                  {list.map((design, i) => (
-                    <DesignThumbnail
-                      key={design._id}
-                      data={design}
-                      index = {i}
-                      center={list.length < 4}
-                    />
-                  ))}
-                </div>
-                )}
-                {list.length >= 4 && (
-                <div className="grid grid-cols-1 sm:grid-cols-6 lg:grid-cols-9 grid-flow-row gap-20 p-4 w-full mb-20 max-w-5xl mx-auto">
-                {list.map((design, i) => (
-                    <DesignThumbnail
-                    key={design._id}
-                    data={design}
-                    index = {i}
-                    center={list.length >= 4}
-                    />
-                ))}
-                </div>
-            )}
-                </>
-            }
-
-        </section>
-    )
-}
+          )}
+          {list.length >= 4 && (
+            <div className="grid grid-cols-1 sm:grid-cols-6 lg:grid-cols-9 grid-flow-row gap-20 p-4 w-full mb-20 max-w-5xl mx-auto">
+              {list.map((design, i) => (
+                <DesignThumbnail
+                  key={design._id}
+                  data={design}
+                  index={i}
+                  center={list.length >= 4}
+                />
+              ))}
+            </div>
+          )}
+        </>
+      )}
+    </section>
+  );
+};
